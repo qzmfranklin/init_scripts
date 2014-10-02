de2e663b2698e611c62a5ab3fe37de1d6b238295
Zhongming Qu <qzmfranklin@gmail.com>
2014-10-02 15:42:45 -0400

    ubuntu install steam

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -5,10 +5,13 @@ sudo apt-get update
 sudo apt-get install build-essential git curl ruby perl vim-gnome tree \
 	python-numpy python-matplotlib python-scipy ipython \
 	python3-numpy python3-matplotlib python3-scipy ipython3 \
-	libfftw3-dev ctags nvidia-current
+	libfftw3-dev ctags
+sudo apt-get install nvidia-current
 sudo apt-get upgrade
 
 sh ./install_vim.sh
 
 echo "source ~/.bash_zhongming" >> ~/.bashrc
 cp bash_zhongming_ubuntu ~/.bash_zhongming
+
+sudo apt-get install steam -y
