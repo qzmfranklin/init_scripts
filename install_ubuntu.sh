27ac84116876fad0145c794fd72509b73ae5e5e8
Zhongming Qu <qzmfranklin@gmail.com>
2014-10-02 15:09:30 -0400

    nvidia driver for ubuntu

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+sudo add-apt-repository -y ppa:xorg-edgers/ppa
 sudo apt-get update
 sudo apt-get install build-essential git curl ruby perl vim-gnome tree \
 	python-numpy python-matplotlib python-scipy ipython \
 	python3-numpy python3-matplotlib python3-scipy ipython3 \
-	libfftw3-dev ctags
+	libfftw3-dev ctags nvidia-current
 sudo apt-get upgrade
 
 sh ./install_vim.sh
