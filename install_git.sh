815ac6c0584cd52a77cebe67ffc940c33e6b6e9e
Zhongming Qu <qzmfranklin@gmail.com>
2015-09-02 14:06:37 -0700

    Better git

--- install_git.sh
+++ install_git.sh
@@ -9,9 +9,6 @@ cat gitignore_global >> ~/.gitignore_global
 # Bash autocomplete
 cp git_bash_completion ~/.git_bash_completion
 
-# Use SSH to avoid having to type passwords
-git config --global url.ssh://git@github.com/.insteadOf https://github.com/
-
 # Default push mode to 'simple': the pre-2.0 behavior
 git config --global push.default simple
 
@@ -20,4 +17,6 @@ git config --global core.mergeoptions --no-edit
 
 # delete all the local branches that are fully merged.
 # Those that aren't fully merge have local changes that would be lost if the branch were to be deleted.
-git config --global alias.pruneLocal '!git fetch -p && git branch -vv | grep '\'': gone]'\'' | awk '\''{print $1}'\'' | xargs git branch -d'
+git config --global alias.prunelocal '!git fetch -p && git branch -vv | grep '\'': gone]'\'' | awk '\''{print $1}'\'' | xargs git branch -d'
+git config --global alias.prunelocal-force '!git fetch -p && git branch -vv | grep '\'': gone]'\'' | awk '\''{print $1}'\'' | xargs git branch -D'
+git config --global alias.tree 'log --graph --oneline --decorate'
