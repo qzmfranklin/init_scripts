4ed8d4f4d887f45dee67d3a291285baf019d7735
Zhongming Qu <qzmfranklin@gmail.com>
2015-09-13 14:05:58 -0700

    Add install_clang.sh

---
+++ install_clang.sh
@@ -0,0 +1,29 @@
+# install clang and llvm to ~/git/clang
+mkdir -p ~/git/clang
+cd ~/git/clang
+
+# install llvm
+svn co http://llvm.org/svn/llvm-project/llvm/trunk llvm
+
+# install clang to llvm/tools
+cd llvm/tools
+svn co http://llvm.org/svn/llvm-project/cfe/trunk clang
+cd -
+
+# install extra clang tools
+cd llvm/tools/clang/tools
+svn co http://llvm.org/svn/llvm-project/clang-tools-extra/trunk extra
+cd -
+
+# install compiler-rt
+cd llvm/projects
+svn co http://llvm.org/svn/llvm-project/compiler-rt/trunk compiler-rt
+cd -
+
+# build llvm and clang
+mkdir build #(in-tree build is not supported)
+cd build
+cmake -G "Unix Makefiles" ../llvm
+make
+#This builds both LLVM and Clang for debug mode.
+#Note: For subsequent Clang development, you can just run make clang.
