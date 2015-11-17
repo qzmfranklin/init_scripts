b7d4db635c8fe35ce681b0cf4a9471b6552a3153
Zhongming Qu <qzmfranklin@gmail.com>
2015-11-16 21:25:49 -0800

    Comment on why install_valgrind_Darwin.sh is completely commented out

--- install_valgrind_Darwin.sh
+++ install_valgrind_Darwin.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# This script is commented out because valgrind is not a standard component on
+# Darwin systems. But it is nice to know how to install valgrind at all, in case
+# we need to install it.
+
 #svn co svn://svn.valgrind.org/valgrind/trunk valgrind
 #cd valgrind
 #./autogen.sh
