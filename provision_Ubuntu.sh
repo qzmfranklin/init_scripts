24ecf82728d5d41ea49bd8e0fdcdbba25ea66cb1
Zhongming Qu <qzmfranklin@gmail.com>
2016-04-06 17:41:03 -0700

    Install icu4c and clang-3.6 for Ubuntu14.04

--- provision_Ubuntu.sh
+++ provision_Ubuntu.sh
@@ -15,6 +15,14 @@ sudo apt-get install -y gnome-tweak-tool
 sudo apt-get install -y vim vim-gnome
 sudo apt-get install -y xclip
 
+# Install ICU4C
+sudo apt-get install -y libicu-dev
+
+# Ubuntu 14.04 only has up to clang-3.6
+sudo apt-get install -y clang-3.6
+sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-3.6 100
+sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-3.6 100
+
 # Install ubuntu-tweak tool for better workspace.
 cd ~/Downloads/ && wget https://launchpad.net/ubuntu-tweak/0.8.x/0.8.5/+download/ubuntu-tweak_0.8.5-1_all.deb
 sudo dpkg -i ~/Downloads/ubuntu-tweak_*.deb; sudo apt-get -f install
