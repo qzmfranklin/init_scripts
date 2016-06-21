1ab19dce69a1c738ff3da55bb656ac233bdb17df
Zhongming Qu <qzmfranklin@gmail.com>
2016-06-21 11:20:16 -0700

    Add xclip to Debian. Add completion for p4 and virsh

--- ansible/roles/bashrc/files/.bashrc.d/alias.d/generic.sh
+++ ansible/roles/bashrc/files/.bashrc.d/alias.d/generic.sh
@@ -3,17 +3,22 @@ case $os in
     "Darwin"*)
         alias eb="mvim ~/.bash_profile"
         alias sb="source ~/.bash_profile"
+        alias l="ls -GF"
+        alias ls="ls -aGF"
+        alias ll="ls -alGhF"
         ;;
     "Linux"*)
 	echo Linux
         alias eb="gvim ~/.bashrc"
         alias sb="source ~/.bashrc"
+        alias l="ls -F --color"
+        alias ls="ls -aF --color"
+        alias ll="ls -alhF --color"
+        alias pbpaste="xclip -selection clipboard"
+        alias pbcopy="xclip -selection clipboard -o"
         ;;
 esac
 
 alias ..="cd .."
 alias up="cd .."
-alias l="ls -GF"
-alias ls="ls -aGF"
-alias ll="ls -alGhF"
 alias ff="find . -name"
