752cf717d9bfec2918d3a8ff40e513754c7f0709
Zhongming Qu <qzmfranklin@gmail.com>
2016-08-09 11:02:40 -0700

    Add ipmi.sh to alias.d

---
+++ ansible/roles/bashrc/files/.bashrc.d/alias.d/ipmi.sh
@@ -0,0 +1,3 @@
+alias sol="ipmitool -I lanplus -U ADMIN -P ADMIN sol activate -H"
+alias pup="ipmitool -I lanplus -U ADMIN -P ADMIN power up -H"
+alias pdown="ipmitool -I lanplus -U ADMIN -P ADMIN power up -H"
