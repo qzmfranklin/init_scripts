b9bc4743ad93c4c193bc50cff56ccd07f0eb7d4f
Zhongming Qu <qzmfranklin@gmail.com>
2017-05-03 00:58:33 -0400

    Install ansible 2.3. There is a nasty bug in ansible 2.0

---
+++ install-ansible.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -ex
+
+arch=$(uname)
+
+if [ $arch == "Linux" ]; then
+    sudo apt-add-repository --yes ppa:ansible/ansible
+    sudo apt update
+    sudo apt install ansible
+elif [ $arch == "Darwin" ]; then
+    echo "Not implemented for Darwin yet."
+else
+    echo "Invalid arch $arch."
+fi
