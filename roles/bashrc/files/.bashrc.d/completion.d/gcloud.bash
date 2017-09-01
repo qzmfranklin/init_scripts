daa1c824f264a11b6b16fe07be7c4de050e0e44b
Zhongming Qu <qzmfranklin@gmail.com>
2017-09-01 10:04:22 -0700

    Add gcloud role for Darwin

---
+++ roles/bashrc/files/.bashrc.d/completion.d/gcloud.bash
@@ -0,0 +1,3 @@
+# This is retrieved from the `brew casks install google-cloud-sdk`.
+source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc
+source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc
