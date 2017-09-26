6192727936124428087257fa438867b86a7ed5b0
Zhongming Qu <qzmfranklin@gmail.com>
2017-09-26 10:23:13 -0700

    Add vim-numbertoggle

--- roles/bashrc/files/.bashrc.d/completion.d/gcloud.bash
+++ roles/bashrc/files/.bashrc.d/completion.d/gcloud.bash
@@ -1,3 +1,3 @@
 # This is retrieved from the `brew casks install google-cloud-sdk`.
-source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc
-source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc
+source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc || true
+source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc || true
