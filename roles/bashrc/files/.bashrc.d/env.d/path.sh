652208e2163e814c1c2b7f26f662de5da07c5446
Zhongming Qu <qzmfranklin@gmail.com>
2018-01-07 00:03:27 -0800

    update clang to 5.0.1

--- roles/bashrc/files/.bashrc.d/env.d/path.sh
+++ roles/bashrc/files/.bashrc.d/env.d/path.sh
@@ -1,3 +1,3 @@
 prepend_path PATH ~/bin
-prepend_path PATH /opt/clang/5.0.0/bin
-prepend_path LD_LIBRARY_PATH /opt/clang/5.0.0/lib
+prepend_path PATH /opt/clang/5.0.1/bin
+prepend_path LD_LIBRARY_PATH /opt/clang/5.0.1/lib
