4277f3c5aa2711b217a8ed639d77e19836203763
Zhongming Qu <qzmfranklin@gmail.com>
2018-01-07 16:04:13 -0800

    Refactor the bashrc paths

---
+++ roles/clang/files/clang.sh
@@ -0,0 +1,2 @@
+prepend_path PATH /opt/clang/5.0.1/bin
+prepend_path LD_LIBRARY_PATH /opt/clang/5.0.1/lib
---
+++ roles/cuda/files/cuda.sh
@@ -0,0 +1,2 @@
+prepend_path PATH /opt/cuda/9.1/bin
+prepend_path LD_LIBRARY_PATH /opt/cuda/9.1/lib64
