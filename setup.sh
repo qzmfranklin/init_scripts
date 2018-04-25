77619909617f777db7e769a93b858b0a0c667bf4
Zhongming Qu <qzmfranklin@gmail.com>
2018-04-25 14:54:13 -0700

    Consolidate into a single script ./setup.sh.
    The goal is that ./setup.sh will install EVERYTHING that a new machine needs.

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+OS=$(uname)
+
+if [[ "$OS" == "Linux" ]]; then
+    sudo apt-add-repository --yes ppa:ansible/ansible
+    sudo apt update
+    sudo apt install -y ansible
+    # The -K flag will prompt you to enter the sudo password.  That is OK.
+    ansible-playbook -i 'localhost,' -c local ubuntu.yml -K
+elif [[ "$OS" == "Darwin" ]]; then
+    which brewn  || \
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    brew update
+    brew install ansible
+    # This should NOT prompt for any password.  DO NOT USE SUDO.
+    ansible-playbook -i 'localhost,' -c local macos.yml
+else
+    echo "Arch $arch is not supported."
+fi
