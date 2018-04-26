1c60b398927cc2ebefa607cfd3be76a434509592
Zhongming Qu <qzmfranklin@gmail.com>
2018-04-25 18:49:05 -0700

    Backport features. Add pre-commit hooks.

--- setup.sh
+++ setup.sh
@@ -1,20 +1,39 @@
-#!/bin/bash
+#!/bin/bash -e
 
+# OS is either Darwin or Linux.
 OS=$(uname)
 
-if [[ "$OS" == "Linux" ]]; then
+
+deb_install_ansible()
+{
     sudo apt-add-repository --yes ppa:ansible/ansible
     sudo apt update
     sudo apt install -y ansible
-    # The -K flag will prompt you to enter the sudo password.  That is OK.
-    ansible-playbook -i 'localhost,' -c local ubuntu.yml -K
-elif [[ "$OS" == "Darwin" ]]; then
+}
+
+mac_install_ansible()
+{
     which brew  || \
-        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+        /usr/bin/ruby -e \
+        "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     brew update
     brew install ansible  &&  brew upgrade ansible
+}
+
+if [[ "$OS" == "Linux" ]]; then
+    which ansible  ||  deb_install_ansible
+    # The -K flag will prompt you to enter the sudo password.  That is OK.
+    DEFAULT_ROLES_PATH=config/init_scripts \
+        ansible-playbook -i 'localhost,' -c local \
+        ubuntu.yml --ask-become-pass \
+        "$@"
+elif [[ "$OS" == "Darwin" ]]; then
+    which ansible  ||  mac_install_ansible
     # This should NOT prompt for any password.  DO NOT USE SUDO.
-    ansible-playbook -i 'localhost,' -c local macos.yml
+    DEFAULT_ROLES_PATH=config/init_scripts \
+        ansible-playbook -i 'localhost,' -c local \
+        macos.yml \
+        "$@"
 else
-    echo "Architecture $arch is not supported."
+    echo "Architecture $OS is not supported."
 fi
