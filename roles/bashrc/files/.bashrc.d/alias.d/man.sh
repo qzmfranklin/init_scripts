3f8f94da3ddfca8893790d13afea6befdd0efb54
Zhongming Qu <qzmfranklin@gmail.com>
2018-04-28 11:16:18 -0700

    Stop using alias function for man

--- roles/bashrc/files/.bashrc.d/alias.d/man.sh
+++ roles/bashrc/files/.bashrc.d/alias.d/man.sh
@@ -1,5 +1,8 @@
-man() {
-  /usr/bin/man $* | \
-    col -b | \
-    vim -MRn -c 'set ft=man nomod nolist nospell nonu' -
-}
+# TODO: Fix the following problem: If vimrc sets tabstop to any number other
+# than 8, the manpage won't display tab-indented texts correctly.
+
+#man() {
+#  /usr/bin/man $* | \
+#    col -b | \
+#    vim -MRn -c 'set ft=man nomod nolist nospell nonu' -
+#}
